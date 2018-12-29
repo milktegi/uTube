376af3df5c788ffe7697ddcef02bed8ee05a6acd
milktegi <milkteegi@gmail.com>
2018-12-29 15:37:45 +0900

    updated state with Fetched data

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,25 @@ import youtube from '../apis/youtube';
 import SearchBar from './SearchBar';
 
 class App extends Component {
-  onTermSubmit = term => {
-    youtube.get('/search', {
+
+  state = { videos: [ ] };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
+    // response.data.items
+    this.setState({ videos: response.data.items }); 
+    // console.log(response);
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        {this.state.videos.length} 개의 영상이 있습니다.
       </div>
     );
   }
