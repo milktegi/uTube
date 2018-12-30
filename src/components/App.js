d297553b34e3edb4394ff87409f244c3369c0ac4
milktegi <milkteegi@gmail.com>
2018-12-30 10:52:07 +0900

    fixed ui with grid

--- src/components/App.js
+++ src/components/App.js
@@ -26,19 +26,27 @@ class App extends Component {
     // console.log('from the app!', video);
     // vItem부터 클릭받고 올라오면 앱 상태 관리를
     // null에서 선택된 비디오 1개로 갱신
-    this.setState({ selectedVideo: video })
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar 
-        onTermSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList 
-        onVselect={this.onVideoSelect} 
-        videos={this.state.videos}/>
-    
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="ten wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="six wide column">
+              <VideoList
+                onVselect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
