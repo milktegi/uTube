fb4c225902af4bf12cde6bd4e6a28fe2d1b49a14
milktegi <milkteegi@gmail.com>
2018-12-30 10:31:59 +0900

    hooked up selectedV

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,11 @@
 import React  from 'react';
 import VideoItem from './VideoItem'
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVselect }) => {
 	// props.videos <= array 
 	// create a video item 
 	const renderedList = videos.map((video)=>{
-		return <VideoItem video={video}/>;
+		return <VideoItem onVselect={onVselect}video={video}/>;
 	})
 	return (
 		<div className="ui relaxed divided list">
